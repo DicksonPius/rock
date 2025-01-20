551b1a19f2ee9f95f91f73e22e38022a87c737b4
code <code@MacBookPro.mshome.net>
2025-01-20 20:53:32 +0000

    Created 2 functions getComputerChoice and getHumanChoice
    The first one is to randomly pick between 1-3 and output a specific message for the number chosen.
    The second calls the accepts user inputs and calls the first function to display random outputs

---
+++ script.js
@@ -0,0 +1,19 @@
+function getComputerChoice(){
+
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+if (randomNumber == 1){
+console.log("Rock");
+}
+else if (randomNumber == 2){
+    console.log("Paper");
+}
+else if (randomNumber == 3){
+    console.log("Scissors");
+}
+}
+//console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let intputs = prompt("Enter preferred option: ", "Rock | Paper | scissors");
+    return getComputerChoice()
+}console.log(getHumanChoice());
